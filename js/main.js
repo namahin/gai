9abe7351ce38f0c7433e6626246ca9857c04abfd
PyFellow <50703361+pyfellow@users.noreply.github.com>
2020-01-14 02:27:46 +0600

    teacher

--- js/main.js
+++ js/main.js
@@ -69,4 +69,20 @@
     document.querySelector('.upload-result').style.display = 'block';
   };
 })();
-// file upload preview
+//code for cover image preview
+function previewImage(event) {
+
+  var reader = new FileReader();
+  var imageField = document.getElementById("cover-img")
+
+  reader.onload = function () {
+
+    if (reader.readyState == 2) {
+      imageField.src = reader.result;
+    }
+
+  }
+
+  reader.readAsDataURL(event.target.files[0]);
+
+}
